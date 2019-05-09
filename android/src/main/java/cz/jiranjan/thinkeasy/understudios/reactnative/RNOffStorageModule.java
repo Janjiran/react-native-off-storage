7d3fa3c04db27feef66af2401f23f425580f5e07
Jan Jiran <johnyjiran@gmail.com>
2019-05-09 15:38:25 +0200

    native library implemented

--- android/src/main/java/cz/jiranjan/thinkeasy/understudios/reactnative/RNOffStorageModule.java
+++ android/src/main/java/cz/jiranjan/thinkeasy/understudios/reactnative/RNOffStorageModule.java
@@ -5,6 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class RNOffStorageModule extends ReactContextBaseJavaModule {
 
@@ -18,5 +24,71 @@ public class RNOffStorageModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNOffStorage";
+  }
+
+  private String resolvePath(String to) {
+    return getReactApplicationContext().getFilesDir().getAbsolutePath() + '/' + to;
+  }
+
+  @ReactMethod
+  public void saveSync(String to, String base64) {
+    try {
+      File f = new File(resolvePath(to));
+      f.getParentFile().mkdirs();
+      FileOutputStream stream = new FileOutputStream(f);
+      try {
+        stream.write(base64.getBytes());
+      } finally {
+        stream.close();
+      }
+    } catch (Exception ignored) {}
+  }
+
+  @ReactMethod
+  public void save(String to, String base64, Promise promise) {
+    try {
+      File f = new File(resolvePath(to));
+      f.getParentFile().mkdirs();
+      FileOutputStream stream = new FileOutputStream(f);
+      try {
+        stream.write(base64.getBytes());
+      } finally {
+        stream.close();
+      }
+      promise.resolve(true);
+    } catch (Exception e) {
+      promise.reject("IO error", e.getMessage());
+    }
+  }
+
+
+  @ReactMethod
+  public void exists(String to, Promise promise) {
+    File f = new File(resolvePath(to));
+    promise.resolve(f.exists());
+  }
+
+  @ReactMethod
+  public void path(String to, Promise promise) {
+    promise.resolve(getReactApplicationContext().getFilesDir().getAbsolutePath() + '/' + to);
+  }
+
+  @ReactMethod
+  public void read(String from, Promise promise) {
+    try {
+      File f = new File(resolvePath(from));
+      FileInputStream fin = new FileInputStream(f);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(fin));
+      StringBuilder sb = new StringBuilder();
+      String line = null;
+      while ((line = reader.readLine()) != null) {
+        sb.append(line);
+      }
+      reader.close();
+      fin.close();
+      promise.resolve(sb.toString());
+    } catch (Exception e) {
+      promise.reject("IO error", e.getMessage());
+    }
   }
 }
--- ios/RNOffStorage.h
+++ ios/RNOffStorage.h
@@ -1,11 +1,13 @@
 
 #if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#import <Foundation/Foundation.h>
 #else
 #import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
 #endif
 
 @interface RNOffStorage : NSObject <RCTBridgeModule>
-
+- (NSString*) resolvePath:(NSString*)name;
 @end
   
